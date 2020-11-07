0969de3c56f9a4100c208a0352e6532d29406662
deds <superman2000129@gmail.com>
2020-11-07 23:53:44 +0300

    Add first part (Checked by norminette + libft-unit-tests

---
+++ ft_substr.c
@@ -0,0 +1,19 @@
+
+#indlude "libft.h"
+/*
+ * Выделяет (с помощью malloc) и возвращает подстроку из строки,
+ * указанной в аргументе.
+ * Подстрока начинается с индекса 'start' и имеет максимальный размер 'len'.
+ */
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char		*substr;
+	size_t		i;
+
+	substr = (char *)malloc(sizeof(*s) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+
+}
+
+

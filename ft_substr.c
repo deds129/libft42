b52004ed09e4d69e9469ecbff2119d81432c0e49
deds <superman2000129@gmail.com>
2020-11-08 21:15:30 +0300

    Add ft_put*.c part

--- ft_substr.c
+++ ft_substr.c
@@ -1,5 +1,5 @@
 
-#include "libft.h"
+#indlude "libft.h"
 /*
  * Выделяет (с помощью malloc) и возвращает подстроку из строки,
  * указанной в аргументе.
@@ -7,31 +7,13 @@
  */
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char				*substr;
-	size_t				i;
-	size_t				j;
+	char		*substr;
+	size_t		i;
 
-	substr = NULL;
-	i = 0;
 	substr = (char *)malloc(sizeof(*s) * (len + 1));
-	if (substr == NULL)
+	if (str == NULL)
 		return (NULL);
-	while (s[i] != '\0')
-	{
-		if (i >= start && j < len)
-		{
-			substr[j] = s[i];
-			j++;
-		}
-		i++;
-	}
-	substr[j] = '\0';
-	return (substr);
+
 }
 
-int main(void)
-{
-	char str1 [30] = "01234567890123456789";
-	printf("%s\n", ft_substr(str1, 2, 10));
-	return (NULL);
-}
+

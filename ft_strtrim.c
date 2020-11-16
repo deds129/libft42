2d020c529f891ed22f9a97b7f13a30a850d0d00c
Hellen Anisha <hanisha@un-e6.kzn.21-school.ru>
2020-11-16 14:49:59 +0300

    add clean version of trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,24 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanisha <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/16 14:42:58 by hanisha           #+#    #+#             */
+/*   Updated: 2020/11/16 14:43:06 by hanisha          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-/*
-
- strnstr + substr
- пишем две функции 1)
-
- set: 12
- s: 112|345|22221
- result: 345
-
- 1)найти индекс первого несовпадения
- 2)найти индекс второго несовпадения
- 3)вычесть длину - 1 несовп 2 несовп
- 3) субстр
- */
-int indexator(char const *s1, char const *set)
+int		indexator(char const *s1, char const *set)
 {
-	size_t i;
-	size_t j;
-	size_t flag;
+	size_t				i;
+	size_t				j;
+	size_t				flag;
 
 	i = 0;
 	while (s1[i])
@@ -28,32 +26,21 @@ int indexator(char const *s1, char const *set)
 		while (set[j])
 		{
 			if (s1[i] == set[j])
-			{
-				j++;
-				break;
-
-			} else
+				break ;
+			else
 			{
 				flag++;
 				j++;
 			}
 		}
 		if (flag >= ft_strlen(set))
-		{
-			return i;
-		}
+			return (i);
 		i++;
 	}
-	return (-1); //?
+	return (-1);
 }
 
-
-/*
-* Выделяет (с помощью malloc (3)) и возвращает копию
-’S1’ с удаленными символами, указанными в ’set’
-от начала и до конца строки.
- */
-int revindexator(char const *s1, char const *set)
+int		revindexator(char const *s1, char const *set)
 {
 	size_t i;
 	size_t j;
@@ -67,44 +54,32 @@ int revindexator(char const *s1, char const *set)
 		while (set[j])
 		{
 			if (s1[i] == set[j])
-			{
-				j++;
-				break;
-
-			} else
+				break ;
+			else
 			{
 				flag++;
 				j++;
 			}
 		}
 		if (flag >= ft_strlen(set))
-		{
-			return i;
-		}
+			return (i);
 		i--;
 	}
-	return (-1); //?
+	return (-1);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char*trimstr;
-	size_t start;
- 	size_t len;
- 	size_t temp;
+	char				*trimstr;
+	size_t				start;
+	size_t				len;
+	size_t				temp;
 
- 	if(!s1 || !set)
+	if (!s1 || !set)
 		return (NULL);
-
-	start = indexator(s1,set);
-	temp = revindexator(s1,set);
+	start = indexator(s1, set);
+	temp = revindexator(s1, set);
 	len = temp - start + 1;
-
-	trimstr = (char *)malloc(sizeof(char *) * len + 1);
-
 	trimstr = ft_substr(s1, start, len);
 	return (trimstr);
 }
-
-
-
